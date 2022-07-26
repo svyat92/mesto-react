7e34e767317042f40b685430191a63a12e1913a8
Svyat Tveritinov <tveritinovsa@gmail.com>
2022-07-26 20:04:20 +0300

    refactor: destructuring props

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -1,15 +1,15 @@
-function ImagePopup(props) {
+function ImagePopup({card, onClose}) {
 
   return (
-    <div className={props.card ? `popup popup_type_image popup_active` : `popup popup_type_image`}>
+    <div className={card ? `popup popup_type_image popup_active` : `popup popup_type_image`}>
       <div className="popup__image-container">
         <button
           type="button"
           className="popup__close-btn link"
-          onClick={props.onClose}
+          onClick={onClose}
         />
-        <img className="popup__image" src={props.card ? props.card.link : ''} alt={props.card ? props.card.name : ''} />
-        <h2 className="popup__desc">{props.card ? props.card.name : ''}</h2>
+        <img className="popup__image" src={card ? card.link : ''} alt={card ? card.name : ''} />
+        <h2 className="popup__desc">{card ? card.name : ''}</h2>
       </div>
     </div>
   );
