d8cfdd36aa08b803a363492bc08d67bc6fd0ae61
Svyat Tveritinov <tveritinovsa@gmail.com>
2022-07-26 20:03:11 +0300

    refactor: form destructuring props

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,12 +1,12 @@
-function PopupWithForm(props) {
-  
+function PopupWithForm({isOpen, name, onClose, title, children}) {
+
   return (
-    <div className={props.isOpen ? `popup popup_type_${props.name} popup_active` : `popup popup_type_${props.name}`}>
+    <div className={isOpen ? `popup popup_type_${name} popup_active` : `popup popup_type_${name}`}>
       <div className="popup__form-container">
-        <button type="button" className="popup__close-btn link" onClick={props.onClose}></button>
-        <h2 className="popup__title">{props.title}</h2>
-        <form action="#" name={props.name} className="form" noValidate>
-          {props.children}
+        <button type="button" className="popup__close-btn link" onClick={onClose}></button>
+        <h2 className="popup__title">{title}</h2>
+        <form action="#" name={name} className="form" noValidate>
+          {children}
           <button type="submit" className="form__submit-btn link" disabled>Сохранить</button>
         </form>
       </div>
